bc60ebb88f9e6a697b67c666b67d4d6c47289673
Evgeniy Klimovich <kliasanik@gmail.com>
2025-05-24 16:40:08 +0300

    add key for taks

--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -16,7 +16,7 @@ export const TodolistItem = (props: Props) => {
          <ul>
             {props.tasks.map(task => {
                return (
-                  <li>
+                  <li key={task.id}>
                      <input type="checkbox" checked={task.isDone}/>
                      <span>{task.title}</span>
                   </li>
