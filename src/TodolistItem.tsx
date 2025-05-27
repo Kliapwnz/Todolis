b6c9a57628ba374a6c69b6bc8c52944ea3878588
Evgeniy Klimovich <kliasanik@gmail.com>
2025-05-27 15:58:08 +0300

    change button x to component Button

--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -25,7 +25,7 @@ export const TodolistItem = ({title, tasks, deleteTask}: Props) => {
                      <li key={task.id}>
                         <input type="checkbox" checked={task.isDone}/>
                         <span>{task.title}</span>
-                        <button onClick={() => deleteTask(task.id)}>x</button>
+                       <Button title={"x"} onClick={()=> deleteTask(task.id)}/>
                      </li>
                   )
                })}
