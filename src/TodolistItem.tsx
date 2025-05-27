6ea94f54105d54e54173854bdd0378b2e129b88f
Evgeniy Klimovich <kliasanik@gmail.com>
2025-05-27 16:08:54 +0300

    add onClick to buttons filters

--- src/TodolistItem.tsx
+++ src/TodolistItem.tsx
@@ -1,13 +1,14 @@
-import {Task} from "./App";
+import {FilterValue, Task} from "./App";
 import {Button} from "./Button";
 
 type Props = {
    title: string
    tasks: Task[]
    deleteTask: (taskId: number) => void
+   changeFilter: (filer:FilterValue) => void
 }
 
-export const TodolistItem = ({title, tasks, deleteTask}: Props) => {
+export const TodolistItem = ({title, tasks, deleteTask, changeFilter}: Props) => {
    return (
       <div>
          <h3>{title}</h3>
@@ -25,16 +26,16 @@ export const TodolistItem = ({title, tasks, deleteTask}: Props) => {
                      <li key={task.id}>
                         <input type="checkbox" checked={task.isDone}/>
                         <span>{task.title}</span>
-                       <Button title={"x"} onClick={()=> deleteTask(task.id)}/>
+                        <Button title={"x"} onClick={() => deleteTask(task.id)}/>
                      </li>
                   )
                })}
             </ul>
          )}
          <div>
-            <Button title={"All"}/>
-            <Button title={"Active"}/>
-            <Button title={"Completed"}/>
+            <Button title={"All"} onClick={()=>changeFilter("all")}/>
+            <Button title={"Active"} onClick={()=>changeFilter("active")}/>
+            <Button title={"Completed"} onClick={()=>changeFilter("completed")}/>
          </div>
       </div>
    );
